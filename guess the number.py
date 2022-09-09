61733a396238cd77a3137f07001d2cd7358ff441
Sam <106349608+Sam-4444@users.noreply.github.com>
2022-09-09 18:59:18 +0430

    Update guess the number.py

--- guess the number.py
+++ guess the number.py
@@ -1,20 +1,20 @@
 import random
-# define a method
-def guess(z):
-    # assign a random number between 0 and z to random_number
-    random_number=random.randint(0,z)
+
+def guess(guess_limit):
+  
+    random_number=random.randint(0,guess_limit)
     guess = None
-    # make a loop
+   
     while guess !=random_number:
         # make sure that the entrance is an integer
         try:
-            guess = input(f"guess a number between 0 and {z}:")
+            guess = input(f"guess a number between 0 and {guess_limit}:")
             guess = int(guess)
         except:
             print(f"{guess} is not an integer")
             exit()
 
-        # conditions
+      
         if guess<random_number:
            print("guess again...too low")
         elif guess>random_number:
@@ -23,5 +23,4 @@ def guess(z):
             print(f"Yeah,that is right , the random number is :{random_number}  hats off to you")
             break
 
-# recall the method
 guess(5)
